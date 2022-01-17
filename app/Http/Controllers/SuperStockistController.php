5e1a563d9aaa2c2d49559ffdb2b9033395a4a3ae
mounitbhandari <tataibhandari@gmail.com>
2022-01-17 23:06:29 +0530

    add super stockist table

---
+++ app/Http/Controllers/SuperStockistController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SuperStockist;
+use App\Http\Requests\StoreSuperStockistRequest;
+use App\Http\Requests\UpdateSuperStockistRequest;
+
+class SuperStockistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSuperStockistRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSuperStockistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SuperStockist  $superStockist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SuperStockist $superStockist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SuperStockist  $superStockist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SuperStockist $superStockist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSuperStockistRequest  $request
+     * @param  \App\Models\SuperStockist  $superStockist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSuperStockistRequest $request, SuperStockist $superStockist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SuperStockist  $superStockist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SuperStockist $superStockist)
+    {
+        //
+    }
+}
