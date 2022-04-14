f96ed37026c908257e0c90511a1527727038c082
priyamghosh1 <priyamghosh73@gmail.com>
2022-04-14 18:12:52 +0530

    updated

--- app/Http/Controllers/RechargeToUserController.php
+++ app/Http/Controllers/RechargeToUserController.php
@@ -4,17 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\RechargeToUserResource;
 use App\Models\RechargeToUser;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RechargeToUserController extends Controller
 {
     public function getTransactionByUser(Request $request){
         $requestedData = (object)$request->json()->all();
-        $data = RechargeToUser::select()
-            ->whereRechargeDoneByUid($requestedData->rechargedByID)
-            ->whereBeneficiaryUid($requestedData->rechargedToID)
-            ->orderBy('created_at', 'desc')
-            ->get();
+
+        $user = User::find($requestedData->rechargedByID);
+
+        if($user->user_type_id === 1){
+            $data = RechargeToUser::select()
+                ->whereBeneficiaryUid($requestedData->rechargedToID)
+                ->orderBy('created_at', 'desc')
+                ->get();
+        }else{
+            $data = RechargeToUser::select()
+                ->whereRechargeDoneByUid($requestedData->rechargedByID)
+                ->whereBeneficiaryUid($requestedData->rechargedToID)
+                ->orderBy('created_at', 'desc')
+                ->get();
+        }
+
+
         return response()->json(['success'=>1,'data'=> RechargeToUserResource::collection($data)], 200);
     }
 
