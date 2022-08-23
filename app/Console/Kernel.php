deab25aea968dd460978831234e496957d7a4f1b
priyamghosh1 <priyamghosh73@gmail.com>
2022-08-23 22:06:45 +0530

    updated cache

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -52,8 +52,8 @@ class Kernel extends ConsoleKernel
 
         //sleep(54);
         $schedule->command('generateTwelveCard:result')->cron('1-59/2 * * * *')->timezone('Asia/Kolkata');
-        $schedule->command('generateSixteenCard:result')->cron('1-59/2 * * * *')->timezone('Asia/Kolkata');
         $schedule->command('generateSingle:result')->cron('1-59/2 * * * *')->timezone('Asia/Kolkata');
+        $schedule->command('generateSixteenCard:result')->cron('1-59/2 * * * *')->timezone('Asia/Kolkata');
 
         //cache files
 //       $schedule->command('config:cache')->dailyAt('00:00')->timezone('Asia/Kolkata');
