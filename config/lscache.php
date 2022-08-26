8092e880a7967d1b8cc5b33278f27a5bddfe0965
priyamghosh1 <priyamghosh73@gmail.com>
2022-08-26 08:54:45 +0530

    updated

--- config/lscache.php
+++ config/lscache.php
@@ -4,18 +4,18 @@ return [
     /**
      * Enable ESI
      */
-    'esi' => env('LSCACHE_ESI_ENABLED', false),
+    'esi' => env('LSCACHE_ESI_ENABLED', true),
 
     /**
      * Default cache TTL in seconds
      */
-    'default_ttl' => env('LSCACHE_DEFAULT_TTL', 0),
+    'default_ttl' => env('LSCACHE_DEFAULT_TTL', 3),
 
     /**
      * Default cache storage
      * private,no-cache,public,no-vary
      */
-    'default_cacheability' => env('LSCACHE_DEFAULT_CACHEABILITY', 'no-cache'),
+    'default_cacheability' => env('LSCACHE_DEFAULT_CACHEABILITY', 'public'),
 
     /**
      * Guest only mode (Do not cache logged in users)
