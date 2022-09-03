f4d018b60c2e56ad1059383e6b0e315ec04b2dc8
priyamghosh1 <priyamghosh73@gmail.com>
2022-09-03 22:27:41 +0530

    updated

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -23,7 +23,12 @@ class Test extends Controller
 
     public function testNew(){
         $set_game_date = Carbon::today()->addDays(1)->format('Y-m-d');
-        return $set_game_date;
+         if((Carbon::today()->format('Y-m-d')) === Carbon::today()->addDays(1)->format('Y-m-d')){
+             $test = true;
+        }else{
+             $test = false;
+         }
+        return $test;
 
 //        $today= Carbon::today()->format('Y-m-d');
 //        $nPlay = PlayMaster::whereDrawMasterId(6)
