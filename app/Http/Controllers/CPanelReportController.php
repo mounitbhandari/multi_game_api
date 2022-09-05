157be1d4d4542e50b362c6de19adccd8005b6172
priyamghosh1 <priyamghosh73@gmail.com>
2022-09-05 13:50:55 +0530

    updated result build barcode

--- app/Http/Controllers/CPanelReportController.php
+++ app/Http/Controllers/CPanelReportController.php
@@ -146,16 +146,16 @@ class CPanelReportController extends Controller
                return  $this->get_total_quantity_by_barcode($detail->play_master_id);
             });
 
-            $detail->prize_value = Cache::remember(((String)$detail->play_master_id).'prize_value', 3000000, function () use ($detail) {
-               return $this->get_prize_value_by_barcode($detail->play_master_id);
-            });
+//            $detail->prize_value = Cache::remember(((String)$detail->play_master_id).'prize_value', 3000000, function () use ($detail) {
+//               return $this->get_prize_value_by_barcode($detail->play_master_id);
+//            });
 
             $detail->amount = Cache::remember(((String)$detail->play_master_id).'amount', 3000000, function () use ($detail) {
                 return $this->get_total_amount_by_barcode($detail->play_master_id);
             });
 
 //            $detail->total_quantity = $this->get_total_quantity_by_barcode($detail->play_master_id);
-//            $detail->prize_value = $this->get_prize_value_by_barcode($detail->play_master_id);
+            $detail->prize_value = $this->get_prize_value_by_barcode($detail->play_master_id);
 //            $detail->amount = $this->get_total_amount_by_barcode($detail->play_master_id);
         }
 
