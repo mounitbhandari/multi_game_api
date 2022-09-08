94f09dab5242a272be4e488944c9406afa6867d1
priyamghosh1 <priyamghosh73@gmail.com>
2022-09-08 22:05:36 +0530

    updated

--- app/Http/Controllers/CPanelReportController.php
+++ app/Http/Controllers/CPanelReportController.php
@@ -67,7 +67,7 @@ class CPanelReportController extends Controller
 
         $data = PlayMaster::select('play_masters.id as play_master_id', DB::raw('substr(play_masters.barcode_number, 1, 8) as barcode_number')
             ,'draw_masters.visible_time as draw_time','draw_masters.id as draw_master_id','play_masters.created_at',
-            'users.email as terminal_pin','play_masters.user_id','play_masters.created_at as ticket_taken_time','play_masters.is_claimed', 'game_types.game_id'
+            'play_masters.user_id','play_masters.created_at as ticket_taken_time','play_masters.is_claimed', 'game_types.game_id'
         )
             ->join('draw_masters','play_masters.draw_master_id','draw_masters.id')
             ->join('users','users.id','play_masters.user_id')
@@ -80,7 +80,7 @@ class CPanelReportController extends Controller
             ->whereRaw('date(play_masters.created_at) >= ?', [$start_date])
             ->whereRaw('date(play_masters.created_at) <= ?', [$end_date])
             ->groupBy('play_masters.id','play_masters.barcode_number',
-                'draw_masters.visible_time','users.email','play_masters.created_at',
+                'draw_masters.visible_time','play_masters.created_at',
                 'play_masters.is_claimed', 'game_types.game_id','draw_masters.id','play_masters.user_id')
             ->orderBy('play_masters.created_at','desc')
             ->get();
