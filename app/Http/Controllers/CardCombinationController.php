20fe31c4a62de6e4baa412eeedad089d77ce7265
priyamghosh1 <priyamghosh73@gmail.com>
2022-09-23 10:15:27 +0530

    twelve card api updated

--- app/Http/Controllers/CardCombinationController.php
+++ app/Http/Controllers/CardCombinationController.php
@@ -11,7 +11,7 @@ class CardCombinationController extends Controller
 
     public function get_all_twelve_card()
     {
-        $data = CardCombination::select('id','rank_name','suit_name','rank_initial')->hereCardCombinationTypeId(1)->get();
+        $data = CardCombination::select('id','rank_name','suit_name','rank_initial')->whereCardCombinationTypeId(1)->get();
         return response()->json(['success'=>1,'data'=> $data], 200,[],JSON_NUMERIC_CHECK);
     }
 
