16e75081232914656e2463d4135d0956abadb0e3
priyamghosh1 <priyamghosh73@gmail.com>
2022-10-23 21:25:33 +0530

    updated

--- app/Http/Controllers/CPanelReportController.php
+++ app/Http/Controllers/CPanelReportController.php
@@ -296,7 +296,7 @@ class CPanelReportController extends Controller
         $return_array = [];
         $data = null;
 
-        $draw_times = DB::select("select draw_master_id from play_masters where date(created_at) = ? order by id",[$today]);
+        $draw_times = DB::select("select draw_master_id from play_masters where date(created_at) = ? order by id desc",[$today]);
 
         foreach ($draw_times as $draw_time){
             $data = null;
