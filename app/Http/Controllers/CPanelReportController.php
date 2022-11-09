4af2a42dc5b7d710c4316341e70d32938149e4a1
priyamghosh1 <priyamghosh73@gmail.com>
2022-11-09 23:31:41 +0530

    uploaded

--- app/Http/Controllers/CPanelReportController.php
+++ app/Http/Controllers/CPanelReportController.php
@@ -93,35 +93,45 @@ class CPanelReportController extends Controller
 
 
             if((Cache::has(((String)$detail->play_master_id).'result')) == 1){
-                $detail->result = Cache::remember(((String)$detail->play_master_id).'result', 3000000, function (){
-                });
+                $detail->result = Cache::get(((String)$detail->play_master_id).'result');
+                $detail->bonus = Cache::get(((String)$detail->play_master_id).'bonus');
             }else{
                 $result = ResultMaster::whereDrawMasterId($detail->draw_master_id)->whereGameDate($detail->created_at->format('Y-m-d'))->whereGameId($detail->game_id)->first();
                 if($result){
                     if($detail->game_id == 1){
                         $resultDetails = ResultDetail::whereResultMasterId($result->id)->whereGameTypeId(2)->first();
                         $showNumber = (NumberCombination::find($resultDetails->combination_number_id))->visible_triple_number;
+                        $bonus = $resultDetails->multiplexer;
                     }else if($detail->game_id == 2){
                         $resultDetails = ResultDetail::whereResultMasterId($result->id)->whereGameTypeId(3)->first();
                         $x = CardCombination::find($resultDetails->combination_number_id);
                         $showNumber = $x->rank_name. ' ' .$x->suit_name;
+                        $bonus = $resultDetails->multiplexer;
                     }else if($detail->game_id == 3){
                         $resultDetails = ResultDetail::whereResultMasterId($result->id)->whereGameTypeId(4)->first();
                         $x = CardCombination::find($resultDetails->combination_number_id);
                         $showNumber = $x->rank_name. ' ' .$x->suit_name;
+                        $bonus = $resultDetails->multiplexer;
                     }else if($detail->game_id == 4){
                         $resultDetails = ResultDetail::whereResultMasterId($result->id)->whereGameTypeId(6)->first();
                         $showNumber = (SingleNumber::find($resultDetails->combination_number_id))->single_number;
+                        $bonus = $resultDetails->multiplexer;
                     }else if($detail->game_id == 5){
                         $resultDetails = ResultDetail::whereResultMasterId($result->id)->whereGameTypeId(7)->first();
                         $showNumber = (DoubleNumberCombination::find($resultDetails->combination_number_id))->visible_double_number;
+                        $bonus = $resultDetails->multiplexer;
                     }
                     $detail->result = Cache::remember(((String)$detail->play_master_id).'result', 3000000, function () use ($showNumber) {
                         return $showNumber;
                     });
+                    $detail->bonus = Cache::remember(((String)$detail->play_master_id).'bonus', 3000000, function () use ($bonus) {
+                        return $bonus;
+                    });
                 }else{
                     $showNumber = "---";
                     $detail->result = $showNumber;
+                    $bonus = "---";
+                    $detail->bonus = $bonus;
                 }
             }
 
