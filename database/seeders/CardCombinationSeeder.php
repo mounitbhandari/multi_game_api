576fcf22e4a57060683547e588d422214e106151
priyamghosh1 <priyamghosh73@gmail.com>
2022-11-10 16:00:51 +0530

    updated card combinations

--- database/seeders/CardCombinationSeeder.php
+++ database/seeders/CardCombinationSeeder.php
@@ -11,44 +11,44 @@ class CardCombinationSeeder extends Seeder
     {
         CardCombination::insert([
 //            12-Card
-            ['rank_name'=>'Jack','suit_name'=>'Club','rank_initial'=>'J', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Jack','suit_name'=>'Diamon','rank_initial'=>'J', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Jack','suit_name'=>'Heart','rank_initial'=>'J', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Jack','suit_name'=>'Spade','rank_initial'=>'J', 'card_combination_type_id' => 1],
+            ['rank_name'=>'J','suit_name'=>'C','rank_initial'=>'J', 'card_combination_type_id' => 1],
+            ['rank_name'=>'J','suit_name'=>'D','rank_initial'=>'J', 'card_combination_type_id' => 1],
+            ['rank_name'=>'J','suit_name'=>'H','rank_initial'=>'J', 'card_combination_type_id' => 1],
+            ['rank_name'=>'J','suit_name'=>'S','rank_initial'=>'J', 'card_combination_type_id' => 1],
 
-            ['rank_name'=>'Queen','suit_name'=>'Club','rank_initial'=>'Q', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Queen','suit_name'=>'Diamon','rank_initial'=>'Q', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Queen','suit_name'=>'Heart','rank_initial'=>'Q', 'card_combination_type_id' => 1],
-            ['rank_name'=>'Queen','suit_name'=>'Spade','rank_initial'=>'Q', 'card_combination_type_id' => 1],
+            ['rank_name'=>'Q','suit_name'=>'C','rank_initial'=>'Q', 'card_combination_type_id' => 1],
+            ['rank_name'=>'Q','suit_name'=>'D','rank_initial'=>'Q', 'card_combination_type_id' => 1],
+            ['rank_name'=>'Q','suit_name'=>'H','rank_initial'=>'Q', 'card_combination_type_id' => 1],
+            ['rank_name'=>'Q','suit_name'=>'S','rank_initial'=>'Q', 'card_combination_type_id' => 1],
 
-            ['rank_name'=>'King','suit_name'=>'Club','rank_initial'=>'K', 'card_combination_type_id' => 1],
-            ['rank_name'=>'King','suit_name'=>'Diamon','rank_initial'=>'K', 'card_combination_type_id' => 1],
-            ['rank_name'=>'King','suit_name'=>'Heart','rank_initial'=>'K', 'card_combination_type_id' => 1],
-            ['rank_name'=>'King','suit_name'=>'Spade','rank_initial'=>'K', 'card_combination_type_id' => 1],
+            ['rank_name'=>'K','suit_name'=>'C','rank_initial'=>'K', 'card_combination_type_id' => 1],
+            ['rank_name'=>'K','suit_name'=>'D','rank_initial'=>'K', 'card_combination_type_id' => 1],
+            ['rank_name'=>'K','suit_name'=>'H','rank_initial'=>'K', 'card_combination_type_id' => 1],
+            ['rank_name'=>'K','suit_name'=>'S','rank_initial'=>'K', 'card_combination_type_id' => 1],
 
 
 
 
 //            16-Card
-            ['rank_name'=>'Jack','suit_name'=>'Club','rank_initial'=>'J', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Jack','suit_name'=>'Diamond','rank_initial'=>'J', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Jack','suit_name'=>'Heart','rank_initial'=>'J', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Jack','suit_name'=>'Spade','rank_initial'=>'J', 'card_combination_type_id' => 2],
+            ['rank_name'=>'J','suit_name'=>'C','rank_initial'=>'J', 'card_combination_type_id' => 2],
+            ['rank_name'=>'J','suit_name'=>'D','rank_initial'=>'J', 'card_combination_type_id' => 2],
+            ['rank_name'=>'J','suit_name'=>'H','rank_initial'=>'J', 'card_combination_type_id' => 2],
+            ['rank_name'=>'J','suit_name'=>'S','rank_initial'=>'J', 'card_combination_type_id' => 2],
 
-            ['rank_name'=>'Queen','suit_name'=>'Club','rank_initial'=>'Q', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Queen','suit_name'=>'Diamond','rank_initial'=>'Q', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Queen','suit_name'=>'Heart','rank_initial'=>'Q', 'card_combination_type_id' => 2],
-            ['rank_name'=>'Queen','suit_name'=>'Spade','rank_initial'=>'Q', 'card_combination_type_id' => 2],
+            ['rank_name'=>'Q','suit_name'=>'C','rank_initial'=>'Q', 'card_combination_type_id' => 2],
+            ['rank_name'=>'Q','suit_name'=>'D','rank_initial'=>'Q', 'card_combination_type_id' => 2],
+            ['rank_name'=>'Q','suit_name'=>'H','rank_initial'=>'Q', 'card_combination_type_id' => 2],
+            ['rank_name'=>'Q','suit_name'=>'S','rank_initial'=>'Q', 'card_combination_type_id' => 2],
 
-            ['rank_name'=>'King','suit_name'=>'Club','rank_initial'=>'K', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Diamond','rank_initial'=>'K', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Heart','rank_initial'=>'K', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Spade','rank_initial'=>'K', 'card_combination_type_id' => 2],
+            ['rank_name'=>'K','suit_name'=>'C','rank_initial'=>'K', 'card_combination_type_id' => 2],
+            ['rank_name'=>'K','suit_name'=>'D','rank_initial'=>'K', 'card_combination_type_id' => 2],
+            ['rank_name'=>'K','suit_name'=>'H','rank_initial'=>'K', 'card_combination_type_id' => 2],
+            ['rank_name'=>'K','suit_name'=>'S','rank_initial'=>'K', 'card_combination_type_id' => 2],
 
-            ['rank_name'=>'King','suit_name'=>'Club','rank_initial'=>'A', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Diamond','rank_initial'=>'A', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Heart','rank_initial'=>'A', 'card_combination_type_id' => 2],
-            ['rank_name'=>'King','suit_name'=>'Spade','rank_initial'=>'A', 'card_combination_type_id' => 2],
+            ['rank_name'=>'A','suit_name'=>'C','rank_initial'=>'A', 'card_combination_type_id' => 2],
+            ['rank_name'=>'A','suit_name'=>'D','rank_initial'=>'A', 'card_combination_type_id' => 2],
+            ['rank_name'=>'A','suit_name'=>'H','rank_initial'=>'A', 'card_combination_type_id' => 2],
+            ['rank_name'=>'A','suit_name'=>'S','rank_initial'=>'A', 'card_combination_type_id' => 2],
 
         ]);
     }
