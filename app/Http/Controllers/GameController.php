b22cf82582e9821bd7a7e3de43d34f6536b4d285
priyamghosh1 <priyamghosh73@gmail.com>
2022-11-13 20:00:11 +0530

    updated

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -402,7 +402,7 @@ class GameController extends Controller
                 $totalPrizeClaimed = $allPlayMaster->is_claimed == 1 ? ($totalPrizeClaimed + $CPanelReportController->get_prize_value_by_barcode($allPlayMaster->id)): $totalPrizeClaimed + 0;
                 $totalPrizeUnclaimed = $allPlayMaster->is_claimed == 0 ? ($totalPrizeUnclaimed + $CPanelReportController->get_prize_value_by_barcode($allPlayMaster->id)): $totalPrizeUnclaimed + 0;
                 $totalBet = $totalBet + $CPanelReportController->total_sale_by_play_master_id($allPlayMaster->id);
-                $totalCommission + $CPanelReportController->get_terminal_commission($allPlayMaster->id);
+                $totalCommission = $totalCommission + $CPanelReportController->get_terminal_commission($allPlayMaster->id);
             }
         }
 
