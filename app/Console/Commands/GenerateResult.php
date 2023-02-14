812f7a3ba564cda71064689c728219cfe7071b91
priyamghosh1 <priyamghosh73@gmail.com>
2023-02-14 09:48:25 +0530

    updated

--- app/Console/Commands/GenerateResult.php
+++ app/Console/Commands/GenerateResult.php
@@ -57,6 +57,45 @@ class GenerateResult extends Command
             $ret = $centralControllerObj->createResult(1,1);
         }
 
+        //12 card
+        $draw_master = DrawMaster::whereActive(1)->whereGameId(2)->first();
+        $min_draw = Carbon::parse($draw_master->end_time)->minute;
+        $day_draw = Carbon::parse($draw_master->end_time)->day;
+        $hour_draw = Carbon::parse($draw_master->end_time)->hour;
+        $min_now = Carbon::now()->minute ;
+        $day_now = Carbon::now()->day ;
+        $hour_now = Carbon::now()->hour ;
+        if(($day_draw === $day_now) && ($min_draw<=$min_now) && ($hour_draw==$hour_now) && (($min_now % $draw_master->time_diff) != 0)){
+            $centralControllerObj = new CentralController();
+            $ret = $centralControllerObj->createResult(1,1);
+        }
+
+        //16 card
+        $draw_master = DrawMaster::whereActive(1)->whereGameId(3)->first();
+        $min_draw = Carbon::parse($draw_master->end_time)->minute;
+        $day_draw = Carbon::parse($draw_master->end_time)->day;
+        $hour_draw = Carbon::parse($draw_master->end_time)->hour;
+        $min_now = Carbon::now()->minute ;
+        $day_now = Carbon::now()->day ;
+        $hour_now = Carbon::now()->hour ;
+        if(($day_draw === $day_now) && ($min_draw<=$min_now) && ($hour_draw==$hour_now) && (($min_now % $draw_master->time_diff) != 0)){
+            $centralControllerObj = new CentralController();
+            $ret = $centralControllerObj->createResult(1,1);
+        }
+
+        //single
+        $draw_master = DrawMaster::whereActive(1)->whereGameId(4)->first();
+        $min_draw = Carbon::parse($draw_master->end_time)->minute;
+        $day_draw = Carbon::parse($draw_master->end_time)->day;
+        $hour_draw = Carbon::parse($draw_master->end_time)->hour;
+        $min_now = Carbon::now()->minute ;
+        $day_now = Carbon::now()->day ;
+        $hour_now = Carbon::now()->hour ;
+        if(($day_draw === $day_now) && ($min_draw<=$min_now) && ($hour_draw==$hour_now) && (($min_now % $draw_master->time_diff) != 0)){
+            $centralControllerObj = new CentralController();
+            $ret = $centralControllerObj->createResult(1,1);
+        }
+
         //double chance
         $draw_master = DrawMaster::whereActive(1)->whereGameId(5)->first();
         $min_draw = Carbon::parse($draw_master->end_time)->minute;
