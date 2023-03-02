65f5cc4fb529376166a86432464b634d3eeb9b71
priyamghosh1 <priyamghosh73@gmail.com>
2023-03-02 19:33:51 +0530

    updated

--- app/Http/Controllers/CPanelReportController.php
+++ app/Http/Controllers/CPanelReportController.php
@@ -285,6 +285,22 @@ class CPanelReportController extends Controller
                 ->get();
             $data['baharNumber'] = $baharNumber;
 
+            $baharNumber = PlayDetails::select('bahar_numbers.bahar_number'
+                ,'play_details.quantity')
+                ->join('bahar_numbers','play_details.combination_number_id','bahar_numbers.id')
+                ->where('play_details.play_master_id',$play_master_id)
+                ->where('play_details.game_type_id',10)
+                ->get();
+            $data['baharNumber'] = $baharNumber;
+
+            $sixteenCard = PlayDetails::select('rollet_numbers.rollet_number',
+                ,'play_details.quantity')
+                ->join('rollet_numbers','play_details.combination_number_id','rollet_numbers.id')
+                ->where('play_details.play_master_id',$play_master_id)
+                ->where('play_details.game_type_id',10)
+                ->get();
+            $data['sixteenCard'] = $sixteenCard;
+
             return $data;
 
         });
